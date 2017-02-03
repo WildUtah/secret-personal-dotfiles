ad25620b72661373ce5f49e4052a84f426bf7f86
Brian <brian@utahbrian.com>
2017-02-02 18:43:49 -0700

    profile and iterm2 as dir

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@
 #note that these have to be added manually. Yuck.
 ln -fs ~/.dot/bashrc ~/.bashrc
 ln -fs ~/.dot/profile ~/.profile
+ln -fs ~/.dot/bash-profile ~/.bash-profile
 ln -fs ~/.dot/lein/profiles.clj ~/.lein/profiles.clj
 ln -fs ~/.dot/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -fs ~/.dot/inputrc ~/.inputrc
 ln -fs ~/.dot/gitconfig ~/.gitconfig
-ln -s ~/.dot/iterm2/com.googlecode.iterm2.plist ~/.iterm2/com.googlecode.iterm2.plist
+ln -fs ~/.dot/iterm2 ~/.iterm2
 
