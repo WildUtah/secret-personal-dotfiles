41a3831f8fe6561b14a54100ecdd51e5eb4ce4db
Brian <brian@utahbrian.com>
2017-02-03 09:45:54 +0000

    prompwd,but needs gcc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd ~/.dot/
 #here are all the dotfiles under control
 #note that these have to be added manually. Yuck.
 ln -fs ~/.dot/bashrc ~/.bashrc
@@ -12,4 +13,11 @@ ln -fs ~/.dot/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -fs ~/.dot/inputrc ~/.inputrc
 ln -fs ~/.dot/gitconfig ~/.gitconfig
 ln -fs ~/.dot/iterm2 ~/.iterm2
+
+if ![[ -x ~/bin/promptpwd ]]; 
+then 
+  gcc -o2 -o promptpwd promptpwd.c
+  strip -s promptpwd
+  mv promptpwd ~/bin/promptpwd
+fi;
 
